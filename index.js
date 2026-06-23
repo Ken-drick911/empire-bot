eb89d35ab60fdd4be2a2d3f1b9a585f069325b87
Ken-drick911 <ambassadorpauldaniel@gmail.com>
2026-06-23 11:43:47 +0100

    Add fetchLatestBaileysVersion to imports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys')
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion } = require('@whiskeysockets/baileys');
 const pino = require('pino')
 const readline = require('readline')
 

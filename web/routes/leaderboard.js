8b0c4c44f85762552f171657ef8e1d6d9f95c8dc
Ken-drick911 <ambassadorpauldaniel@gmail.com>
2026-06-24 22:58:17 +0100

    Refactor database connection method in leaderboard route

--- web/routes/leaderboard.js
+++ web/routes/leaderboard.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getDB } = require('../../src/data/db')
+const getDB = () => global._db
 
 router.get('/', async (req, res) => {
     try {

6a4816cdcd5c436d8768710148de9e5ef047e6b9
Ken-drick911 <ambassadorpauldaniel@gmail.com>
2026-06-24 22:07:45 +0100

    Initialize Express server with routes and middleware

---
+++ web/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const { connectDB } = require('../src/data/db')
+
+const authRoutes = require('./routes/auth')
+const profileRoutes = require('./routes/profile')
+const shopRoutes = require('./routes/shop')
+
+const app = express()
+const PORT = process.env.WEB_PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api/auth', authRoutes)
+app.use('/api/profile', profileRoutes)
+app.use('/api/shop', shopRoutes)
+
+// Serve frontend
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`🌐 Empire Web running on port ${PORT}`)
+    })
+})
+
+module.exports = app

3f266106147e119115f655e7e4f85a5f0887ec5f
Ken-drick911 <ambassadorpauldaniel@gmail.com>
2026-06-28 18:34:20 +0100

    Update stats route to count groups from 'groups' collection

--- web/routes/stats.js
+++ web/routes/stats.js
@@ -5,10 +5,14 @@ const getDB = () => global._db
 router.get('/', async (req, res) => {
   try {
     const db = getDB()
+
     const citizens = await db.collection('users')
       .countDocuments({ id: { $regex: '@s.whatsapp.net' } })
-    const groups = await db.collection('users')
-      .distinct('groupId').then(r => r.length).catch(() => 0)
+
+    const groups = await db.collection('groups')
+      .countDocuments()
+      .catch(() => 0)
+
     res.json({ citizens, legions: groups, victories: 0 })
   } catch (err) {
     res.status(500).json({ error: 'Server error' })

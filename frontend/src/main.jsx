f2afc07bfb5be1315b13c73346eae63f8a2f27dd
Ken-drick911 <ambassadorpauldaniel@gmail.com>
2026-06-30 17:48:18 +0100

    Import atmosphere.css for theme styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './theme/tokens.css'
+import "./theme/atmosphere.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
